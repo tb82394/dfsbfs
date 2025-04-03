95c7ad0c1e5ff823e5e7217ab7e0db45e87162fa
Thomas <tb82394@lasallebajio.edu.mx>
2025-04-03 10:26:45 -0600

    clase Arbol

--- src/App.java
+++ src/App.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class App {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-          
+          scanner.close();
     }
 }
--- src/Arbol.java
+++ src/Arbol.java
@@ -2,4 +2,19 @@ import java.util.ArrayList;
 
 public class Arbol {
     private ArrayList<Nodo> nodos = new ArrayList<>();
+
+    public Arbol() {
+
+    }
+    
+    public void dfs() {}
+    public void bfs() {}
+
+    public ArrayList<Nodo> getNodos() {
+        return nodos;
+    }
+
+    public void setNodos(ArrayList<Nodo> nodos) {
+        this.nodos = nodos;
+    }
 }
